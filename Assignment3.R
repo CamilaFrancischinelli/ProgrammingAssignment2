c0a95c781021b40f0e3b8143cd3c711f7059da19
CamilaFrancischinelli <45465699+CamilaFrancischinelli@users.noreply.github.com>
2019-01-15 21:04:12 +0100

    Add files via upload

--- Assignment3.R
+++ Assignment3.R
@@ -1,16 +1,23 @@
+# This function sets the parameters of the list that will be created and
+# the variables that will be called in a later stage to calculate the 
+# inverse of the given marix
+
 makeCacheMatrix <- function(x = matrix()) {
-       inv <- NULL
+       inv <- NULL                            # sets the inverse to NULL before starting the calculations
        set <- function(y) {
               x <<- y
-              inv <<- NULL
+              inv <<- NULL                    # sets the inverse to NULL before starting the calculations
        }
        get <- function() x
-       setsolve <- function() inv <<- solve(x) #calculates the inverse of matrix x
+       setsolve <- function() inv <<- solve(x) # calculates the inverse of matrix x
        getsolve <- function() inv
        list(set = set, get = get,
             setsolve = setsolve,
-            getsolve = getsolve)
+            getsolve = getsolve)               
 }
+
+
+# This is the function where the inverse of matrix x is calculated
 
 cacheSolve <- function(x, ...) {
        inv <- x$getsolve()
@@ -19,7 +26,7 @@ cacheSolve <- function(x, ...) {
               return(inv)
        }
        data <- x$get()
-       inv <- solve(data, ...)
+       inv <- solve(data, ...) # the inverse of matrix x is stored in inv 
        x$setsolve(inv)
        inv
 }
